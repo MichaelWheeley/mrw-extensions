2aedf75cbd9d9cab173ba61e0d0daa10ed9270ab
MichaelWheeley <MichaelWheeley@users.noreply.github.com>
2021-03-06 14:30:48 -0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+MRW.setupGreenPurple(strip)
+MRW.rotateForever(strip,100)
